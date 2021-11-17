9c751d4835e2971a7542b444e99feea8ff32b556
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2021-11-17 14:54:20 -0600

    remove Chakar

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Button, ChakraProvider } from '@chakra-ui/react';
+// import React from 'react';
+// import { Button, ChakraProvider } from '@chakra-ui/react';
 
-export default {
-  title: 'Chakra/Button',
-  component: Button,
-  decorators: [
-    (Story) => (
-      <ChakraProvider>
-        <Story />
-      </ChakraProvider>
-    ),
-  ],
-};
+// export default {
+//   title: 'Chakra/Button',
+//   component: Button,
+//   decorators: [
+//     (Story) => (
+//       <ChakraProvider>
+//         <Story />
+//       </ChakraProvider>
+//     ),
+//   ],
+// };
 
-export const Success = () => <Button colorScheme="green">Success</Button>;
-export const Danger = () => <Button colorScheme="red">Danger</Button>;
+// export const Success = () => <Button colorScheme="green">Success</Button>;
+// export const Danger = () => <Button colorScheme="red">Danger</Button>;
