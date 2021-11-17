8d3af1e402bed5e07d047aa7790da6fea1b93c54
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2021-11-17 10:52:23 -0600

    [4] --> Renaming & Sorting <--

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 export default {
-  title: 'Form/Button',
+  title: 'Theme/Form/Button',
   component: Button,
 };
 
@@ -10,3 +10,4 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
